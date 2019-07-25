cad5c19529cb57595eb16868dced8e82884116be
bee25141 <bee25141@gmail.com>
2019-07-24 19:12:03 -0500

    jumbotron rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Component} from "react";
+import Jumbotron from "./components/Jumbotron/Jumbotron"
 import ImgCard from "./components/ImgCard/ImgCard"
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    clicked: false,
+    clickMatch: 0
+  }
+  render() {
+    return (
+      <div>
+      <Jumbotron />
+
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ my-app/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Style.css"
+
+function Jumbotron(){
+    return (
+    <div className="jumbotron text-center">
+    <h1>Clicky Game</h1>
+    
+  </div>
+    );
+};
+
+export default Jumbotron;
